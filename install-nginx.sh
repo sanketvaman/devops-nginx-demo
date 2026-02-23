d059d4a55b6c6d1d1756b26d49fae75b7375c0f5
sanketvaman <vamansanket5@gmail.com>
2026-02-23 11:57:10 +0000

    chore: minor changes to nginx script

---
+++ install-nginx.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#This is a script to install  and enable nginx
+
+sudo apt-get update -y
+
+sudo apt install nginx -y
+
+sudo systemctl start nginx
+
+sudo systemctl enable nginx
+
+echo "NGINX Installed"
